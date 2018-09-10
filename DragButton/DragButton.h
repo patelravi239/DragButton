ad65a25df7cd4846998c1671030d1e052b0ca3be
Ravinandan Patel <ravinandan@edcast.com>
2018-09-10 12:42:17 +0530

    Initial Commit

---
+++ DragButton/DragButton.h
@@ -0,0 +1,19 @@
+//
+//  DragButton.h
+//  DragButton
+//
+//  Created by RAVI on 10/09/18.
+//  Copyright © 2018 ravi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DragButton.
+FOUNDATION_EXPORT double DragButtonVersionNumber;
+
+//! Project version string for DragButton.
+FOUNDATION_EXPORT const unsigned char DragButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DragButton/PublicHeader.h>
+
+
